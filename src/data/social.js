f70176fc7f00a81dd63194901bb9d9ae816679c9
Tyler Bainbridge <tyler@tylerbainbridge.com>
2019-12-14 17:56:02 -0500

    Update social links

--- src/data/social.js
+++ src/data/social.js
@@ -1,16 +1,35 @@
-import { FaLinkedinIn, FaRegEnvelope, FaGithub } from "react-icons/fa";
+import {
+  FaLinkedinIn,
+  FaRegEnvelope,
+  FaGithub,
+  FaInstagram,
+  FaSpotify,
+  FaLastfmSquare
+} from 'react-icons/fa';
 
 export default [
   {
-    url: "mailto:tyler@tylerbainbridge.com",
+    url: 'mailto:tyler@tylerbainbridge.com',
     Icon: FaRegEnvelope
   },
   {
-    url: "https://github.com/tylerjbainbridge",
+    url: 'https://github.com/tylerjbainbridge',
     Icon: FaGithub
   },
   {
-    url: "https://www.linkedin.com/in/tylerbainbridge",
+    url: 'https://www.linkedin.com/in/tylerbainbridge',
     Icon: FaLinkedinIn
+  },
+  {
+    url: 'https://www.instagram.com/tylerjbainbridge/',
+    Icon: FaInstagram
+  },
+  {
+    url: 'https://www.last.fm/user/tylerbainbridge',
+    Icon: FaLastfmSquare
+  },
+  {
+    url: 'https://open.spotify.com/user/1286951915?si=74ZnWWCST_GcpCQMCqKGfw',
+    Icon: FaSpotify
   }
 ];
