0d382ccbb1d4705a43d4c993e8d1bd1ea02289a1
tbainbridge <tbainbridge@cargurus.com>
2019-12-16 13:31:09 -0500

    revert to old behavior

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,60 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { StaticQuery, graphql, Link } from "gatsby";
+import { StaticQuery, graphql } from "gatsby";
+import Helmet from "react-helmet";
 
-import styled, {
-  css,
-  ThemeProvider,
-  createGlobalStyle,
-} from "styled-components";
-import { Location } from "@reach/router";
+import styled, { ThemeProvider } from "styled-components";
+
 import "../fonts/fonts.css";
 
 import { BASE_THEME, LIGHT_THEME, DARK_THEME } from "../util/constants";
 
-import { InlineButton, InlineLink } from "../components/UI";
-
-const GlobalStyle = createGlobalStyle`
-  body { background-color: ${props => props.theme.backgroundColor}; }
-
-  html.wf-loading * {
-    opacity: 0;
-  }
-`;
-
-const Sticky = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-grow: 1;
-  width: 100vw;
-  top: 0;
-  left: 0;
-  position: fixed;
-  z-index: 10;
-
-  @media (max-width: 768px) {
-    background-color: ${props => props.theme.backgroundColor};
-  }
-`;
-
-export const StickyPart = styled.span`
-  padding: 20px;
-`;
+import { InlineButton } from "../components/UI";
 
 const Layout = ({ children }) => {
-  const [isDark, setDark] = useState(
-    typeof window !== "undefined" && window.localStorage.getItem("isDark")
-      ? JSON.parse(window.localStorage.getItem("isDark") || undefined)
-      : undefined
-  );
-
-  useEffect(() => {
-    window.localStorage.setItem("isDark", isDark);
-  }, [isDark]);
+  const [isDark, setDark] = useState(false);
 
   const theme = {
     ...BASE_THEME,
-    ...(isDark ? DARK_THEME : LIGHT_THEME),
+    ...(isDark ? DARK_THEME : LIGHT_THEME)
   };
 
   return (
@@ -68,49 +30,45 @@ const Layout = ({ children }) => {
           }
         }
       `}
-      render={data =>
-        isDark !== undefined && (
-          <Location>
-            {({ location }) =>
-              console.log(location) || (
-                <ThemeProvider theme={theme}>
-                  <Sticky>
-                    <StickyPart>
-                      {location.pathname !== "/" && (
-                        <InlineLink to='/'>Home</InlineLink>
-                      )}
-                      {location.pathname !== "/blog" && (
-                        <InlineLink to='/blog'>Blog</InlineLink>
-                      )}
-                    </StickyPart>
-                    <StickyPart>
-                      <InlineButton onClick={() => setDark(!isDark)}>
-                        {isDark ? "Light" : "Dark"} theme
-                      </InlineButton>
-                    </StickyPart>
-                  </Sticky>
-                  <GlobalStyle whiteColor />
-                  <PageContainer>{children}</PageContainer>
-                </ThemeProvider>
-              )
-            }
-          </Location>
-        )
-      }
+      render={data => (
+        <ThemeProvider theme={theme}>
+          <>
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+              <InlineButton onClick={() => setDark(!isDark)}>
+                {isDark ? "Light" : "Dark"} theme
+              </InlineButton>
+            </div>
+            <Helmet>
+              <style>{`
+                    body { background-color: ${theme.backgroundColor} }
+
+                    html.wf-loading * {
+                      opacity: 0;
+                    }
+                  `}</style>
+            </Helmet>
+            <PageContainer>{children}</PageContainer>
+          </>
+        </ThemeProvider>
+      )}
     />
   );
 };
 
+const SiteWrapper = styled.div`
+  ${"" /* display: flex;
+  flex-direction: row;
+  justify-content: center; */}
+`;
+
 const PageContainer = styled.div`
   margin: 0 auto;
-  max-width: 700px;
-  padding: 10px;
+  max-width: 900px;
   margin-bottom: 150px;
-  margin-top: 60px;
 `;
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
 };
 
 export default Layout;
